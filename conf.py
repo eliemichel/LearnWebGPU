c4539be726ad7874a823fce837fd4caf4566b991
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2025-01-15 15:32:23 +0800

    Add Chinese

--- conf.py
+++ conf.py
@@ -195,4 +195,5 @@ translation_languages = [
     ("fr", "🇫🇷", "Français", "French"),
     ("kr", "🇰🇷", "한국인", "Korean"),
     ("it", "🇮🇹", "Italiano", "Italian"),
+    ("zh", "🇨🇳", "中文", "Chinese"),
 ]
